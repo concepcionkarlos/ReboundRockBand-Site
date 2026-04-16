d0b24c1cc64445a87196dd63a58fdce5cb1d508c
Juan Carlos Concepcion Ametller <concepcionkarlos@gmail.com>
2026-04-15 22:49:33 -0400

    MerchGallery: switch from next/image to native img for reliable display on Vercel

--- src/components/merch/MerchGallery.tsx
+++ src/components/merch/MerchGallery.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { useState } from 'react'
 
 interface Props {
@@ -15,15 +14,14 @@ export default function MerchGallery({ images, name }: Props) {
     <div className="flex flex-col gap-2">
       {/* Main image */}
       <div className="relative aspect-square bg-brand-elevated overflow-hidden border border-brand-border group-hover:border-brand-red/30 transition-colors duration-300">
-        <Image
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
           key={images[active]}
           src={images[active]}
           alt={name}
-          fill
-          className="object-cover"
-          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
-          priority={active === 0}
+          className="w-full h-full object-cover transition-transform duration-500 ease-out group-hover:scale-[1.03]"
         />
+        <div className="absolute left-0 top-0 bottom-0 w-[2px] bg-brand-red origin-bottom scale-y-0 group-hover:scale-y-100 transition-transform duration-300" />
       </div>
 
       {/* Thumbnails */}
@@ -39,12 +37,11 @@ export default function MerchGallery({ images, name }: Props) {
                   : 'border-brand-border hover:border-brand-red/40'
               }`}
             >
-              <Image
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
                 src={src}
                 alt={`${name} view ${i + 1}`}
-                fill
-                className="object-cover"
-                sizes="56px"
+                className="w-full h-full object-cover"
               />
             </button>
           ))}
